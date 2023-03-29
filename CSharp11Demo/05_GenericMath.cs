ac8c8205928976b7be4eba169dd085c78c5d7e1b
Robert Haken <haken@havit.cz>
2023-03-30 00:10:47 +0200

    Generic Math

---
+++ CSharp11Demo/05_GenericMath.cs
@@ -0,0 +1,26 @@
+﻿using System.Numerics;
+
+namespace GenericMath;
+
+public class Demo
+{
+	public int Add(int a, int b) => a + b;
+	public decimal Add(decimal a, decimal b) => a + b;
+	public double Add(double a, double b) => a + b;
+
+	public T Add<T>(T a, T b)
+		where T : INumber<T>
+	{
+		return a + b;
+	}
+
+	public T Divide<T>(T a, T b)
+		where T : INumber<T>
+	{
+		if (b == T.Zero)
+		{
+			throw new DivideByZeroException();
+		}
+		return a / b;
+	}
+}
